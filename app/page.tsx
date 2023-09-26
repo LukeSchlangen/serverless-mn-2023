7dcab6686048876e9c8350bf366c3d6e105f3117
Luke Schlangen <lukeschlangen@gmail.com>
2023-09-25 19:11:56 -0500

    add message to DOM

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,21 @@
 
 import { useState } from 'react';
 
+type Message = {
+  userName: string,
+  text: string,
+}
+
 export default function Home() {
   const [displayName, setDisplayName] = useState('');
   const [messageText, setMessageText] = useState('');
+  const [messageFeed, setMessageFeed] = useState<Message[]>([]);
   const sendMessage = () => {
     const newMessage = {
       userName: displayName,
       text: messageText,
     };
-    console.log(newMessage);
+    setMessageFeed([...messageFeed, newMessage]);
   }
   return (
     <main>
@@ -18,7 +24,11 @@ export default function Home() {
       <input onChange={(e) => setDisplayName(e.target.value)} value={displayName} />
       <input onChange={(e) => setMessageText(e.target.value)} value={messageText} />
       <button onClick={sendMessage}>Send Message</button>
-
+      <ul>
+        {messageFeed.map((message) => (<li key={message.text}>
+          {message.userName}: {message.text}
+        </li>))}
+      </ul>
       <p>Monkey Capybara Flour Flower</p>
     </main>
   )
