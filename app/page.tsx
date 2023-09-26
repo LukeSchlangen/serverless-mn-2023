82cd778883c1537f5dbddefb541d8a96c13544b4
Luke Schlangen <lukeschlangen@gmail.com>
2023-09-25 19:05:58 -0500

    add messaging functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
-import Image from 'next/image'
+"use client"
+
+import { useState } from 'react';
 
 export default function Home() {
+  const [displayName, setDisplayName] = useState('');
+  const [messageText, setMessageText] = useState('');
+  const sendMessage = () => {
+    const newMessage = {
+      userName: displayName,
+      text: messageText,
+    };
+    console.log(newMessage);
+  }
   return (
     <main>
       <h1>Serverless MN</h1>
-      
+      <input onChange={(e) => setDisplayName(e.target.value)} value={displayName} />
+      <input onChange={(e) => setMessageText(e.target.value)} value={messageText} />
+      <button onClick={sendMessage}>Send Message</button>
+
       <p>Monkey Capybara Flour Flower</p>
     </main>
   )
